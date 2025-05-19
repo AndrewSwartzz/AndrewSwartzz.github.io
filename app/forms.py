9a977bf9821b4150ebda46bcf344499b7f798af8
andrew_swartzz <andreww_swartzz@gmail.com>
2025-05-19 16:11:06 -0400

    added project

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,2 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-    TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
-    Length
-import sqlalchemy as sa
-from app import db
-from app.models import User
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
 
